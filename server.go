0974d73eab7ce2ba43c06fef24ea0b8a48ef2deb
Guilherme Martins <academicgmag@gmail.com>
2026-01-09 14:15:58 -0300

    chore: addying env vars

--- server.go
+++ server.go
@@ -1,12 +1,18 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
+	"os"
 )
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("<h1>Hello, World!</h1>"))
+		name := os.Getenv("NAME")
+		planet := os.Getenv("PLANET")
+
+		fmt.Fprintf(w, "<h1>Hello, World!</h1>\n <p>I am %s, from %s</p>", name, planet)
 	})
+
 	http.ListenAndServe(":80", nil)
 }
