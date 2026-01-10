373608353af63fad38dcdaedce8f36e84581a6e8
Guilherme Martins <academicgmag@gmail.com>
2026-01-10 09:11:31 -0300

    chore: using secrets

--- server.go
+++ server.go
@@ -14,5 +14,14 @@ func main() {
 		fmt.Fprintf(w, "<h1>Hello, World!</h1>\n <p>I am %s, from %s</p>", name, planet)
 	})
 
+	http.HandleFunc("/secret", Secret)
+
 	http.ListenAndServe(":80", nil)
+}
+
+func Secret(w http.ResponseWriter, r *http.Request) {
+	user := os.Getenv("USER")
+	password := os.Getenv("PASSWORD")
+
+	fmt.Fprintf(w, "<h1>Secret</h1>\n <p>User: %s, Password: %s</p>", user, password)
 }
